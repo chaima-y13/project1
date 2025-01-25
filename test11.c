d4287eb5816b36bc69e196ddf6f2e220126014bf
chaima-y13 <kacemchaima13@gmail.com>
2025-01-25 21:26:28 +0100

    modefied file

---
+++ test11.c
@@ -0,0 +1,21 @@
+# include<stdio.h>
+int main () {
+    // calculaton de superficie et perimetre d un;
+     int l,lr,s,p;
+    printf(" calculer le perimetre \n");
+    printf("veuillez saiser  la langueur de rectangle \t");
+    scanf("%d",&l);
+    printf(" veuillez saiser le largeur de rectangle ");
+    scanf("%d",&lr);
+    p=(l+lr)*2;
+    s=l*lr;
+    printf(" le perimetre est : %d\n",p);
+    printf(" la superficie est %d",s);
+    return 0;
+   
+  
+
+
+
+    
+}
