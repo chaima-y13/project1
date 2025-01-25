08eb89da385415fb6778dd11686991fcbf7805a1
chaima-y13 <kacemchaima13@gmail.com>
2025-01-25 02:57:44 +0100

    modified testtwo.c

---
+++ testtwo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main () {
+    int x,y,n;
+    // code multiplation
+    printf("donner deux nombre pour les multiplier\t");
+    scanf("%d",x);
+    scanf("%d",y);
+    n=x*y;
+    printf(" la multiplication  est: %d",n);
+    return 0;
+}
