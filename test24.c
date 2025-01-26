9808dbdb96963fade4af28c79d1d6c4c6fe3cf08
chaima-y13 <kacemchaima13@gmail.com>
2025-01-26 14:33:22 +0100

    modefied test24

---
+++ test24.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    // affichage de quelque chose 30 foix 
+    int i;
+    for (i=1;i<=30;i++)
+    {
+        printf("1: tu es belle\n");
+
+    }
+    return 0;
+    
+}
