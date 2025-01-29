0c544e4c8e8c1faba257dde3c67290622ead9dab
chaima-y13 <kacemchaima13@gmail.com>
2025-01-29 11:51:30 +0100

    modified test38

---
+++ test38.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () {
+    //la table de multiplication de 5
+    int i,p;
+    printf("la table de multiplication de 5");
+    for(i=1;i<=10;i++)
+    { p=5*i;
+    printf("5*%d=%d",i,p);
+    p=0;
+        
+    }
+    return 0;
+}
