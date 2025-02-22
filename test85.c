9e44d1d8f735b82c252090606f1c1061540835d8
chaima-y13 <kacemchaima13@gmail.com>
2025-02-22 20:40:31 +0100

    modified t85 and 84exe

---
+++ test85.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+// fibonatchi normal 
+int main () {
+ int i,uj=0,ui=1,n,un;
+  do {
+ printf("enter number of term superior then 2");
+scanf("%d",&n);
+}
+while (n<2) ;
+for (i=2;i<=n;i++)
+{
+    un=uj+ui;
+    printf("%d\t",un);
+    uj=ui;
+    ui=un;
+}
+return 0;
+}
