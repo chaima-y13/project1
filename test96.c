9bf8c10aca48be1b0ecdf467a8714afc76fbfa7b
chaima-y13 <kacemchaima13@gmail.com>
2025-03-12 13:20:36 +0100

    modified test96

---
+++ test96.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// premiere seance tp les pointerus 
+// declaration des pointeurs int float char string tableau dentiers record 
+// declaration de record student 
+// this exercice is just for training how to use pointers 
+typedef struct student {
+    char name [30];
+    int age;
+    char fname [40];
+}student;
+int main () {
+    int n ; int *p=&n;
+    float o,*i=&o; 
+    char e,*cc=&e; int
+    t[10],*t1[10]=&t[10]; 
+    char name[30],*r=&name;
+    student etd,*eps=&etd;
+return 0; 
+
+}
